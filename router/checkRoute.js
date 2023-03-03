9e20a9c4347330c016d7a7946618eaff7ce4ef1c
azizguennichi <ezzoguennichi@gmail.com>
2023-03-03 15:29:27 +0100

    first commit

---
+++ router/checkRoute.js
@@ -0,0 +1,8 @@
+import { check } from "../controllers/checkController.js";
+import express from "express";
+
+const checkRoute = express.Router();
+
+checkRoute.post("/add", check);
+
+export default checkRoute;
